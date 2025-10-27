c42b738c0ecf71157a2aa0d5deb9104bf3a9ca64
dkerkove <dankerkove@gmail.com>
2025-10-27 13:45:47 -0500

    add initial files

---
+++ LoginDemo/LoginDemo/LoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginDemoApp.swift
+//  LoginDemo
+//
+//  Created by Dan Kerkove on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginDemo/LoginDemoTests/LoginDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginDemoTests.swift
+//  LoginDemoTests
+//
+//  Created by Dan Kerkove on 10/27/25.
+//
+
+import Testing
+@testable import LoginDemo
+
+struct LoginDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
